b1be2970fc955af86dd0a017d773b7f9bac45285
Ariss <valentina.ways@yandex.ru>
2020-12-05 15:18:21 +0300

    chanded parameters to main function

--- src/Differ.php
+++ src/Differ.php
@@ -3,30 +3,41 @@
 namespace Gendiff\Differ;
 
 use function Funct\Strings\startsWith;
-use function Funct\Collection\rest;
 use function Funct\Collection\forEvery;
 
-function getDifference($argv)
+function getFullPath($file)
 {
-    $files = rest($argv);
-    
-    $fullPathsToFiles = forEvery($files, function($file) {
-        if (!startsWith($file, "/")) {
-            $cwd = getcwd();
-            $fullPathToFile = "{$cwd}/{$file}";
-        } else {
-            $fullPathToFile = $file;
-        }
-        return $fullPathToFile;
-    });
+    if (!startsWith($file, "/")) {
+        $cwd = getcwd();
+        $fullPathToFile = "{$cwd}/{$file}";
+    } else {
+        $fullPathToFile = $file;
+    }
+    return $fullPathToFile;
+}
 
-    $firstFile = json_decode(file_get_contents($fullPathsToFiles[0]), true);
-    $secondFile = json_decode(file_get_contents($fullPathsToFiles[1]), true);
+
+function formatResult(array $array)
+{
+    $jsonEncoded = json_encode($array);
+    $droppedQuotes = preg_replace('/"/', "", $jsonEncoded);
+    $addedLineBreaks = preg_replace('/}/', "\n$0\n", preg_replace(['/{/', '/,/'], "$0\n", $droppedQuotes));
+    return $addedLineBreaks;
+}
+
+function getDifference($firstFile, $secondFile)
+{
+    
+    $fullPathToFirstFile = getFullPath($firstFile);
+    $fullPathToSecondFile = getFullPath($secondFile);
+
+    $firstFile = json_decode(file_get_contents($fullPathToFirstFile), true);
+    $secondFile = json_decode(file_get_contents($fullPathToSecondFile), true);
 
     $keys = array_merge(array_keys($firstFile), array_keys($secondFile));
     sort($keys);
     $differences = array_reduce($keys, function($acc, $key) use ($firstFile, $secondFile) {
-        
+        $keyUnmodified = "  {$key}";
         $keyAdded = "+ {$key}";
         $keyDeleted = "- {$key}";
 
@@ -41,11 +52,12 @@ function getDifference($argv)
                 $acc[$keyAdded] = $secondFile[$key];
             }
         } else {
-            $acc[$key] = $firstFile[$key];
+            $acc[$keyUnmodified] = $firstFile[$key];
         }
 
         return $acc;
     }, []);
     
-    print_r(json_encode($differences));
+    $formattedResult = formatResult($differences);
+    print_r($formattedResult);
 }
