26f7e192f0cf4e4579e58dfbe961bea498894a06
Ariss <valentina.ways@yandex.ru>
2020-12-10 16:36:19 +0300

    created tests for parser

---
+++ tests/ParserTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Gendiff\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Gendiff\Parser;
+
+class ParserTest extends TestCase
+{
+    public function testParse()
+    {
+        $pathToJsonFile = __DIR__ . '/fixtures/TestDoc1.json';
+        $pathToYamlFile = __DIR__ . '/fixtures/TestDoc1.yaml';
+        $arrayOfData = [
+            "host" => "hexlet.io",
+            "timeout" => 50,
+            "proxy" => "123.234.53.22",
+            "follow" => false
+        ];
+        $expected = (object) $arrayOfData;
+        $this->assertEquals($expected, parse($pathToJsonFile));
+        $this->assertEquals($expected, parse($pathToYamlFile));
+    }
+}
