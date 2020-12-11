e2bc3a868e4fb0ff460155eca26f67391e1c824c
Ariss <valentina.ways@yandex.ru>
2020-12-11 16:42:21 +0300

    changed differ tests for complex files

--- tests/DifferTest.php
+++ tests/DifferTest.php
@@ -19,12 +19,50 @@ class DifferTest extends TestCase
     public function testFormatResult()
     {
         $docBefore = [
-            "host" => "hexlet.io",
-            "timeout" => 50,
-            "proxy" => "123.234.53.22",
-            "follow" => false
+            "  common" => [
+                "+ follow" => false,
+                "  setting1" => "Value 1",
+                "- setting2" => 200,
+                "- setting3" => true,
+                "+ setting3" => null,
+                "+ setting4" => "blah blah",
+                "+ setting5" => [
+                    "key5" => "value5"
+                ],
+                "  setting6" => [
+                    "  doge" => [
+                        "- wow" => null,
+                        "+ wow" => "so much",
+                    ],
+                    "  key" => "value",
+                    "+ ops" => "vops"
+                ],
+            ],
+            "group1" => [
+                "- baz" => "bas",
+                "+ baz" => "bars",
+                "  foo" => "bar",
+                "- nest" => [
+                    "  key" => "value"
+                ],
+                "+ nest" => "str"
+            ],
+            "- group2" => [
+                "abc" => 12345,
+                "deep" => [
+                    "id" => 45
+                ]
+            ],
+            "+ group3" => [
+                "fee" => 100500,
+                "deep" => [
+                    "id" => [
+                        "number" => 45
+                    ]
+                ]
+            ]
         ];
-        $docAfter = file_get_contents(__DIR__ . '/fixtures/TestDoc3.txt');
+        $docAfter = file_get_contents(__DIR__ . '/fixtures/Result1.txt');
         $this->assertEquals($docAfter, Differ\formatResult($docBefore));
     }
 
@@ -39,7 +77,7 @@ class DifferTest extends TestCase
 
     public function additionProvider()
     {
-        $docAfter = file_get_contents(__DIR__ . '/fixtures/TestDoc4.txt');
+        $docAfter = file_get_contents(__DIR__ . '/fixtures/Result1.txt');
         return [
             [$docAfter,  __DIR__ . '/fixtures/TestDoc1.json', __DIR__ . '/fixtures/TestDoc2.json'],
             [$docAfter,  __DIR__ . '/fixtures/TestDoc1.yaml', __DIR__ . '/fixtures/TestDoc2.yaml']
