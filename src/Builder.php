ad45754ea1d9030f88f7b698ed186c9ea741debc
Ariss <valentina.ways@yandex.ru>
2020-12-24 16:30:26 +0300

    added default for children in createNode

--- src/Builder.php
+++ src/Builder.php
@@ -10,14 +10,14 @@ function getPropertiesNames(object $object): array
     return array_keys(get_object_vars($object));
 }
 
-function createNode(string $key, string $type, $children, $oldValue, $newValue): array
+function createNode(string $key, string $type, $oldValue, $newValue, $children = null): array
 {
     return [
         "key" => $key,
         "type" => $type,
-        "children" => $children,
         "oldValue" => $oldValue,
-        "newValue" => $newValue
+        "newValue" => $newValue,
+        "children" => $children
     ];
 }
 
@@ -28,18 +28,18 @@ function buildDiff(object $firstData, object $secondData): array
 
     $differences = array_map(function ($key) use ($firstData, $secondData) {
         if (!property_exists($firstData, $key)) {
-            return createNode($key, "added", [], null, $secondData->$key);
+            return createNode($key, "added", null, $secondData->$key);
         }
         if (!property_exists($secondData, $key)) {
-            return createNode($key, "deleted", [], $firstData->$key, null);
+            return createNode($key, "deleted", $firstData->$key, null);
         }
         if ($firstData->$key !== $secondData->$key) {
             if (is_object($firstData->$key) && is_object($secondData->$key)) {
-                return createNode($key, "complex", buildDiff($firstData->$key, $secondData->$key), null, null);
+                return createNode($key, "complex", null, null, buildDiff($firstData->$key, $secondData->$key));
             }
-                return createNode($key, "changed", [], $firstData->$key, $secondData->$key);
+                return createNode($key, "changed", $firstData->$key, $secondData->$key);
         }
-            return createNode($key, "unchanged", [], $firstData->$key, $secondData->$key);
+            return createNode($key, "unchanged", $firstData->$key, $secondData->$key);
     }, $sortedKeys);
 
     return $differences;
