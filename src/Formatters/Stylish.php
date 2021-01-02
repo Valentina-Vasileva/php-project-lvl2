60e98a3372bf1643f2b9492caa6c66d9b752fad4
Ariss <valentina.ways@yandex.ru>
2021-01-02 17:08:27 +0300

    levelSpaces -> indent

--- src/Formatters/Stylish.php
+++ src/Formatters/Stylish.php
@@ -16,9 +16,9 @@ function stringify($value, int $level): string
         $keys = array_keys(get_object_vars($value));
 
         $formattedArrayOfValue = array_map(function ($key) use ($value, $level) {
-            $levelSpaces = str_repeat(" ", ($level + 1) * 4);
+            $indent = str_repeat(" ", ($level + 1) * 4);
             $childValue = stringify($value->$key, $level + 1);
-            return "{$levelSpaces}{$key}: {$childValue}";
+            return "{$indent}{$key}: {$childValue}";
         }, $keys);
 
         $formattedValue = implode("\n", $formattedArrayOfValue);
@@ -38,25 +38,25 @@ function formatToStylish($data, int $level): string
 {
     $formattedArray = array_map(function ($node) use ($level) {
 
-        $levelSpaces = str_repeat(" ", ($level - 1) * 4);
+        $indent = str_repeat(" ", ($level - 1) * 4);
 
         $formattedOldValue = stringify($node['oldValue'], $level);
         $formattedNewValue = stringify($node['newValue'], $level);
 
         switch ($node["type"]) {
             case ("added"):
-                return "{$levelSpaces}  + {$node['key']}: {$formattedNewValue}";
+                return "{$indent}  + {$node['key']}: {$formattedNewValue}";
             case ("deleted"):
-                return "{$levelSpaces}  - {$node['key']}: {$formattedOldValue}";
+                return "{$indent}  - {$node['key']}: {$formattedOldValue}";
             case ("changed"):
-                $addedNode = "{$levelSpaces}  + {$node['key']}: {$formattedNewValue}";
-                $deletedNode = "{$levelSpaces}  - {$node['key']}: {$formattedOldValue}";
+                $addedNode = "{$indent}  + {$node['key']}: {$formattedNewValue}";
+                $deletedNode = "{$indent}  - {$node['key']}: {$formattedOldValue}";
                 return implode("\n", [$deletedNode, $addedNode]);
             case ("complex"):
                 $children = formatToStylish($node["children"], $level + 1);
-                return "{$levelSpaces}    {$node['key']}: {$children}";
+                return "{$indent}    {$node['key']}: {$children}";
             case ("unchanged"):
-                return "{$levelSpaces}    {$node['key']}: {$formattedNewValue}";
+                return "{$indent}    {$node['key']}: {$formattedNewValue}";
             default:
                 throw new \Exception("The node type '{$node['type']}' is not identified");
         }
