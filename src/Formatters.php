3ed3d148e811253e09e6327a806210caa0ac20e6
Ariss <valentina.ways@yandex.ru>
2021-01-11 17:28:00 +0300

    added flag for exception trow for json

--- src/Formatters.php
+++ src/Formatters.php
@@ -13,11 +13,7 @@ function format(array $data, string $format): string
         case ('plain'):
             return Plain\format($data);
         case ('json'):
-            $formattedData = json_encode($data);
-            if (json_last_error() !== JSON_ERROR_NONE) {
-                throw new \Exception(json_last_error_msg());
-            }
-            return $formattedData;
+            return json_encode($data, JSON_THROW_ON_ERROR);
         default:
             throw new \Exception("The report format '{$format}' is not supported");
     }
--- src/Parsers.php
+++ src/Parsers.php
@@ -8,11 +8,7 @@ function parse(string $data, string $format): object
 {
     switch ($format) {
         case ('json'):
-            $parsedData = json_decode($data);
-            if (json_last_error() !== JSON_ERROR_NONE) {
-                throw new \Exception(json_last_error_msg());
-            }
-            return $parsedData;
+            return json_decode($data, false, 512, JSON_THROW_ON_ERROR);
         case ('yaml'):
         case ('yml'):
             return Yaml::parse($data, Yaml::PARSE_OBJECT_FOR_MAP);
